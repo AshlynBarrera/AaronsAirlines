60a52fc4193d505522c0fcecea92ac319da86b01
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-01-13 09:03:21 -0600

    Initial Commit

---
+++ AaronsAirlines/AaronsAirlinesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AaronsAirlinesApp.swift
+//  AaronsAirlines
+//
+//  Created by Ashlyn Barrera on 1/13/23.
+//
+
+import SwiftUI
+
+@main
+struct AaronsAirlinesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
